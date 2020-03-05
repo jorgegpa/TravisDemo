21b40af8d4af5655017ca8f720268d4254c7d48b
vn03iim <vn03iim@mx.walmart.com>
2020-03-05 13:11:21 -0600

    agregando prueba

--- test/testTravis.js
+++ test/testTravis.js
@@ -4,5 +4,9 @@ describe('Array', function () {
         it('should return -1 when the value is not present', function () {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
+
+        it('should return 1 at the position 2',function(){
+            assert.equal([1,2,3].indexOf(2),1);
+        });
     });
 });
